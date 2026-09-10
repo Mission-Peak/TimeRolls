9e065463c1dd8c4d02cad3c2157bd716d115f0f3
Hanna <hanna@attimis.com>
2026-09-10 14:20:15 -0400

    Initial Commit

---
+++ Photo Chronology/Photo_ChronologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_ChronologyApp.swift
+//  Photo Chronology
+//
+//  Created by Hanna Mehraby on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Photo_ChronologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
